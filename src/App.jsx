5b056bcdd8b00a8fbcd38540a02c11cb7c21fdce
sunny <sunny.s6191@gmail.com>
2024-05-23 13:28:01 +0530

    Add new files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import TopHeader from './layouts/TopHeader';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+import Router from './router/Router';
+
+function App() {
+    return (
+        <>
+            <TopHeader />
+
+            <Header />
+            
+            <Router />
+            
+            <Footer />
+        </>
+    );
+}
+
+export default App;
