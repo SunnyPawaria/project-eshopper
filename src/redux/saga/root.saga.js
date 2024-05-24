e824331ca2ef2849f351900b54aef4aa0c78e3f0
sunny <sunny.s6191@gmail.com>
2024-05-24 23:03:12 +0530

    Added redux

---
+++ src/redux/saga/root.saga.js
@@ -0,0 +1,10 @@
+import { all, fork } from "redux-saga/effects";
+import { category } from "./category.saga";
+
+function* root() {
+    return yield all([
+        fork(category)
+    ])
+}
+
+export default root;
