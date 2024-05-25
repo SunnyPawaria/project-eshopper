e122070f18e3afcea8df835c5a46e25670fab6df
sunny <sunny.s6191@gmail.com>
2024-05-25 23:33:24 +0530

    Added admin files

---
+++ src/pages/admin/sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Sidebar() {
+    return (
+        <ul className="list-group">
+            <li className="list-group-item active" aria-current="true">Menu</li>
+            <li className="list-group-item">
+                <Link to='/admin'>Profile</Link> 
+            </li>
+            <li className="list-group-item">
+                <Link to='/admin/orders'>Orders</Link> 
+            </li>
+            <li className="list-group-item">
+                <Link to='/admin/products'>Product</Link> 
+            </li>
+            <li className="list-group-item">
+                <Link to='/admin/category'>Category</Link> 
+            </li>
+            <li className="list-group-item">
+                <Link to='/admin/users'>User</Link> 
+            </li>          
+        </ul>
+    )
+}
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,21 +3,53 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Category from '../pages/Category'
 import Details from '../pages/Details'
-import Login from '../pages/Login'
 import Register from '../pages/Register'
+import Login from '../pages/Login'
 import CartDetails from '../pages/cart/CartDetails'
 import Checkout from '../pages/cart/Checkout'
+import Auth from '../pages/admin/Auth'
+import Profile from '../pages/admin/Profile'
+import Product from '../pages/admin/products/Product'
+import AddOrEdit from '../pages/admin/products/AddOrEdit'
+import AdminCategory from '../pages/admin/category/Category'
+import AddOrUpdate from '../pages/admin/category/AddOrUpdate'
+import Order from '../pages/admin/orders/Order'
+import View from '../pages/admin/orders/View'
+import User from '../pages/admin/users/User'
+import UserAddOrUpdate from '../pages/admin/users/AddOrUpdate'
+
+
 
 export default function Router() {
   return (
     <Routes>
-        <Route path='checkout' element={<Checkout />} />
-        <Route path='/cart' element={<CartDetails/>}/>
-        <Route path='/register' element={<Register />}  />
-        <Route path='/login' element={<Login />}  />
-        <Route path='/details' element={<Details />}  />
-        <Route path='/category' element={<Category />}  />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/cart' element={<CartDetails />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/details' element={<Details />} />
+        <Route path='/category' element={<Category />} />
         <Route path="/" element={<Home />} />
+
+        <Route path='/admin' element={<Auth />}>
+            <Route index={true} element={<Profile />} /> 
+            
+            <Route path="products" element={<Product />} /> 
+            <Route path="products/add" element={<AddOrEdit />} /> 
+            <Route path="products/edit/:id" element={<AddOrEdit />} /> 
+
+            <Route path="category" element={<AdminCategory />} /> 
+            <Route path="category/add" element={<AddOrUpdate />} /> 
+            <Route path="category/edit/:id" element={<AddOrUpdate />} /> 
+
+            <Route path="orders" element={<Order />} /> 
+            <Route path="orders/:id" element={<View />} /> 
+
+            <Route path="users" element={<User />} /> 
+            <Route path="user/add" element={<UserAddOrUpdate />} /> 
+            <Route path="user/edit/:id" element={<UserAddOrUpdate />} /> 
+
+        </Route>
     </Routes>
   )
 }
